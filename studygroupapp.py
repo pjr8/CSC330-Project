1dc90c16eb5839d7b58bc0c4c58de7548dbe232a
Bianka Edouard <bedouard2005@outlook.com>
2026-05-01 15:29:56 -0400

    removed second debug=true that stopped app from running

--- studygroupapp.py
+++ studygroupapp.py
@@ -21,4 +21,4 @@ def profile():
     return render_template("profile.html", user=user)
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=8080, debug=True)
+    app.run(debug=True, host="0.0.0.0", port=8080)
