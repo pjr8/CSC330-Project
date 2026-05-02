c11a7d068543912075a89d88fda2a4cee9d66a01
raza443 <Razas2@southernct.edu>
2026-05-01 21:26:55 -0400

    Add messages route to render messages.htmlAdded messages page route
    Added a new route to render messages.html.

--- studygroupapp.py
+++ studygroupapp.py
@@ -1,5 +1,4 @@
-from flask import Flask
-
+from flask import Flask, render_template
 
 def create_app() -> Flask:
     app = Flask(__name__)
@@ -8,11 +7,14 @@ def create_app() -> Flask:
     def index() -> str:
         return "Study Group App"
 
+    @app.route("/messages")
+    def messages():
+        return render_template("messages.html")
+
     return app
 
 
 app = create_app()
-
 
 if __name__ == "__main__":
     app.run(debug=True)
